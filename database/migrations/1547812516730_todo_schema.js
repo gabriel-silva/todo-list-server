770403070e36b16a8f689e80413715c19f188cab
Gabriel Neves <jose.gabriel@percent.com.br>
2019-01-18 10:36:53 -0300

    adicionando método de registrar um usuário / route AuthController

---
+++ database/migrations/1547812516730_todo_schema.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TodoSchema extends Schema {
+  up() {
+    this.create('todos', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('title', 50).notNullable()
+      table.string('content', 250).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('todos')
+  }
+}
+
+module.exports = TodoSchema
